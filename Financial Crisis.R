0c5bf114d1f7824c094133ec89ec51c4688e981c
Qin Yu <qin.yu.15@ucl.ac.uk>
2018-06-30 22:17:54 +0100

    Investigating Stock Price Dynamics 2

--- Financial Crisis.R
+++ Financial Crisis.R
@@ -1,11 +1,13 @@
-rm(list=ls(all=TRUE)) 
+rm(list=ls(all=TRUE))
 library("corrgram")
 #setwd("H:/...")
 
 ###################################################################################
 ###### Reading and preparing data from csv file downloaded from Wharton:
 
-dataraw<-read.csv("data/JPM WFC BAC C GS USB COF AXP INTC SI IBM MSFT BHGE FE XOM AEIS.csv")
+dataraw <- read.csv("data/JPM WFC BAC GS USB COF AXP INTC SI IBM MSFT BHGE FE XOM AEIS.csv")
+dataadd <- read.csv("data/MA.csv")
+dataraw <- rbind(dataraw, dataadd)
 names(dataraw) 
 ids<-unique(dataraw[,1])
 tks<-unique(dataraw[,3])
@@ -20,19 +22,61 @@ for (tik in ids) {
 data<-matrix(NA,nObs,nAssets+1)
 data[,1]<-dataraw[dataraw[,1]==ids[1],2]  # array[condition, no.]
 for (i in 1:nAssets){
-  data[,i+1]<-dataraw[dataraw[,1]==ids[i],3]
+  data[,i+1]<-dataraw[dataraw[,1]==ids[i],4]
 }
 time<-as.Date(as.character(data[,1]),"%Y%m%d")
-ticker<-c() ######### here to be updated
+ticker<-as.character(tks)
+colnames(data)[2:(nAssets+1)]<-ticker
+
+rearranged_data <- matrix(NA,nObs,nAssets+1)
+rearranged_data[,  1] <- data[, 1]
+rearranged_data[,  9:12 + 1] <- data[, c( 1,  3,  8, 15) + 1]
+rearranged_data[, 13:16 + 1] <- data[, c( 2,  4, 11, 13) + 1]
+rearranged_data[,  1: 8 + 1] <- data[, c( 5,  6,  7,  9, 10, 12, 14, 16) + 1]
+data <- rearranged_data
+
+rearranged_tckr <- vector('numeric', nAssets)
+rearranged_tckr[ 9:12] <- ticker[c( 1,  3,  8, 15)]
+rearranged_tckr[13:16] <- ticker[c( 2,  4, 11, 13)]
+rearranged_tckr[ 1: 8] <- ticker[c( 5,  6,  7,  9, 10, 12, 14, 16)]
+ticker <- rearranged_tckr
 colnames(data)[2:(nAssets+1)]<-ticker
 
 ###################################################################################
 ###### Plot the stock price dynamics:
 
-par(mfrow=c(3,2))
-for (i in 1:nAssets){
-  plot(time,data[,(i+1)],ylab=ticker[i],xlab="", type = "l", col = i)
+#par(mfrow=c(4,4))
+#for (i in 1:nAssets){
+#  plot(time[127:1134],data[127:1134,(i+1)],ylab=ticker[i],xlab="", type = "l", col = i)
+#  title(paste("Fig.2.", i, " Stock Price of ", ticker[i], sep = ""))
+#}
+
+average_data <- numeric(nrow(data))
+par(mfrow=c(3,3))
+for (i in 1:8){
+  plot(time[127:1134],data[127:1134,(i+1)],ylab=ticker[i],xlab="", type = "l", col = i)
+  title(paste("Fig.2.", i, " Stock Price of ", ticker[i], sep = ""))
+  average_data <- average_data + data[,i + 1]
 }
+average_data <- average_data/8
+plot(time[127:1134], average_data[127:1134], ylab = "Avg", xlab = "", type = "l", col = 1)
+title("Fig.2.x Average Stock Price")
+
+par(mfrow=c(2,2))
+for (i in 9:12){
+  plot(time[127:1134],data[127:1134,(i+1)],ylab=ticker[i],xlab="", type = "l", col = i)
+  title(paste("Fig.3.", i-8, " Stock Price of ", ticker[i], sep = ""))
+}
+
+par(mfrow=c(2,2))
+for (i in 13:16){
+  plot(time[127:1134],data[127:1134,(i+1)],ylab=ticker[i],xlab="", type = "l", col = i)
+  title(paste("Fig.4.", i-12, " Stock Price of ", ticker[i], sep = ""))
+}
+
+
+
+#data <- data[127:1134,]
 
 ###################################################################################
 ###### Calculate and plot the log return:
@@ -45,7 +89,8 @@ prices <- prices[2:(T+1),]  # abandon the first day for alignment
 logprices <- logprices[2:(T+1),]
 
 dev.new()
-plot(time[2:length(time)], logprices[,1],type="l",ylim=c(min(logprices),max(logprices)),ylab="")
+plot(time[2:length(time)], logprices[,1], type="l", ylim=c(min(logprices), max(logprices)), xlab = "", ylab="")
+title("Fig.1 Overview: Log Prices of All 16 Stocks")
 for (i in 2:ncol(logprices)) {
   lines(time[2:length(time)], logprices[,i],type="l",col=i)
 }
